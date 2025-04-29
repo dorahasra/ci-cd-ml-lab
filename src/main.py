27d2d70330a68ef27a2f6464d083cf2e534aee90
dorahasra <dorahasra@graduate.utm.my>
2025-04-29 10:38:49 +0800

    Set up CI/CD pipeline with main.py, test_main.py, and testing step

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 # src/main.py
 import pandas as pd
-78
+
 def load_data():
- # Example DataFrame
- data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
- return pd.DataFrame(data)
+    # Example DataFrame
+    data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+    return pd.DataFrame(data)
+
